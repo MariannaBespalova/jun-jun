364289ac626b88d88e349dbc8417ffde1741b3a9
marianna bespalova <mariannabespalova9@gmail.com>
2021-04-23 11:58:41 +0300

    extra task

--- ht2/js/index.js
+++ ht2/js/index.js
@@ -20,3 +20,15 @@ let sum = '10' + 10;
 console.log(sum, typeof sum); //1010 string, преобразование типов данных
 
 
+//________________
+function findNeedle(haystack) {
+  if (haystack.includes("needle")){
+
+    let index = haystack.indexOf('needle');
+    return `found the needle at position ${index}`;
+  }
+}
+
+
+
+findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
