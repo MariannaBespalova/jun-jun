6fc00f879549a1b65b1e10c5306b5f48ebfe5fea
marianna bespalova <mariannabespalova9@gmail.com>
2021-04-23 11:23:40 +0300

    ht2

---
+++ ht2/js/index.js
@@ -0,0 +1,22 @@
+let admin;
+let someName;
+
+someName = "John";
+admin = someName;
+
+alert(admin);
+
+
+// ______________
+
+let ourPlanet = "Earth";
+let currentUserName = "John";
+
+
+//________________
+
+let sum = '10' + 10;
+
+console.log(sum, typeof sum); //1010 string, преобразование типов данных
+
+
