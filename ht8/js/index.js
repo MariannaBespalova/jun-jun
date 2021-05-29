5fba33e3a8449b82c162724357010ba9f55b3451
marianna <mariannabespalova9@gmail.com>
2021-05-29 09:30:22 +0300

    addition tasks

--- ht8/js/index.js
+++ ht8/js/index.js
@@ -1,13 +1,13 @@
 // Запросить у пользователя 2 числа (число, степень) после чего вывести ему результат во всплывающем окне
 
 function powNum(mathOperation) {
-  let num = +prompt("Введите число");
-  let degree = +prompt("Введите степень числа");
-  if (typeof num === "number" &&
-    typeof degree === "number" &&
-    !Number.isNaN(num) &&
-    !Number.isNaN(degree)) {
-    alert(powFunc(num, degree))
+  let num1 = +prompt("Введите число");
+  let num2 = +prompt("Введите степень числа");
+  if (typeof num1 === "number" &&
+    typeof num2 === "number" &&
+    !Number.isNaN(num1) &&
+    !Number.isNaN(num2)) {
+    alert(mathOperation(num1, num2))
   } else {
     alert("Only numbers allowed");
   }
@@ -18,4 +18,21 @@ function powFunc(num, degree) {
   return num ** degree;
 }
 
+function multiplay(num1, num2) {
+  return num1 * num2;
+}
+
+function division(num1, num2) {
+  return num1 / num2;
+}
+
+function modulus(num1, num2) {
+  return `Remainder of ${num1} and ${num2} is ${num1 % num2}`;
+}
+
 powNum(powFunc);
+powNum(multiplay);
+powNum(division);
+powNum(modulus);
+
+
